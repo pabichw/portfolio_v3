fc445ab18e5ec12f2df31eb1c93fd3e7669de32d
Wiktor Pabich <pabichwiktor@gmail.com>
2021-01-23 22:20:38 +0100

    fix(deploy): webpack resolve added

--- next.config.js
+++ next.config.js
@@ -1,4 +1,9 @@
 const withSass = require('@zeit/next-sass')
-module.exports = withSass({webpack: (config, {dir, dev, isServer, buildId, defaultLoaders}) => {
-    return config
-}})
+const path = require('path')
+
+module.exports = withSass({
+    webpack: (config, {dir, dev, isServer, buildId, defaultLoaders}) => {
+        config.resolve.modules.push(path.resolve('./'))
+        return config
+    }
+})
