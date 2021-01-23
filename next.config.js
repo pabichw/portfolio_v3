bb909f8ebd9c626358204e74c28dd8ed750ef6e0
Wiktor Pabich <pabichwiktor@gmail.com>
2021-01-23 22:38:02 +0100

    fix(deploy): webpack resolver alias

--- next.config.js
+++ next.config.js
@@ -4,6 +4,7 @@ const path = require('path')
 module.exports = withSass({
     webpack: (config, {dir, dev, isServer, buildId, defaultLoaders}) => {
         config.resolve.modules.push(path.resolve('./src'))
+        config.resolve.alias['@'] = path.join(__dirname, 'src')
         return config
     }
 })
