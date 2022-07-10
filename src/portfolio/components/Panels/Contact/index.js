cd0496d50724ee1e7bc00d6c7c9c04269b05246c
Wiktor Pabich <pabichwiktor@gmail.com>
2022-07-10 19:27:46 +0200

    fix: facts as grid

--- src/portfolio/components/Panels/Contact/index.js
+++ src/portfolio/components/Panels/Contact/index.js
@@ -1,5 +1,4 @@
 import React, {Component} from 'react';
-
 import Zoom from 'react-reveal/Zoom';
 
 const EMAIL = 'pabichwiktor@gmail.com'
@@ -10,7 +9,7 @@ class Contact extends Component {
       <Zoom>
         <div id='contact' className='contact-panel-wrapper'>
           <div className='ending-wrapper'>
-            <p>Want to get in touch? Cool.<br/>Please use following email address:</p>
+            <p>Want to get in touch? Cool.<br/>Please use the following email address:</p>
             <a href={"mailto:" + EMAIL} target="_top" className="email primary-text">{EMAIL}</a>
           </div>
         </div>
